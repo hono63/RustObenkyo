363661a92dd56551cc22f41d6c05a982904b8594
hono63 <hono63@penguin>
2021-04-29 01:09:17 +0900

    u

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 3;
+    println!("Hello, world{}!", a);
     print_str("Hello str, String.");
+    matching();
+    vector_for();
 }
 
 fn print_str(s0: &str) {
@@ -8,4 +11,21 @@ fn print_str(s0: &str) {
     let s2: &str = &s1;
     let s3: String = s2.to_string();
     println!("{}", s3);
+}
+
+fn matching() {
+    let ret:  Result<i32, String> = Ok(200);
+    let _rete: Result<i32, String> = Err("sippai".to_string());
+    match ret {
+        Ok(okkee) => println!("ok:{}", okkee),
+        Err(eraa) => println!("error:{}", eraa),
+    };
+}
+
+fn vector_for() {
+    let mut v = vec![1, 2, 3];
+    v.push(9);
+    for elem in &v {
+        println!("v:{}", elem);
+    }
 }
